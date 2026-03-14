b56823c16b605e3876eb98ea252ec2919653c2e0
Josue Chaves <josuchazu.jc@gmail.com>
2026-03-13 22:16:49 -0400

    fix experience mocks

--- src/features/breadcrumb/constants/breadcrumb.constants.js
+++ src/features/breadcrumb/constants/breadcrumb.constants.js
@@ -6,7 +6,7 @@ export const BREADCRUMB_PATHS = {
   [FILE_IDS.PROJECTS]: ['josue-chaves', 'src', 'projects.js'],
   [FILE_IDS.SKILLS]: ['josue-chaves', 'data', 'skills.json'],
   [FILE_IDS.EXPERIENCE]: ['josue-chaves', 'src', 'experience.ts'],
-  [FILE_IDS.CONTACT]: ['josue-chaves', 'src', 'contact.css'],
+  [FILE_IDS.CONTACT]: ['josue-chaves', 'src', 'contact.css  '],
   [FILE_IDS.README]: ['josue-chaves', 'README.md'],
 };
 
